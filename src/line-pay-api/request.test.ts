214f95f030cceb357e79337bd61de7ca4ea884f5
Sean Lin <sean.enylin@gmail.com>
2021-11-30 00:41:43 +0800

    test(api): test request api

---
+++ src/line-pay-api/request.test.ts
@@ -0,0 +1,46 @@
+import { request, RequestRequestBody } from './request'
+
+const mockHttpClient = {
+  get: jest.fn(),
+  post: jest.fn()
+}
+
+describe('request', () => {
+  const body: RequestRequestBody = {
+    amount: 100,
+    currency: 'TWD',
+    orderId: 'orderId',
+    packages: [
+      {
+        id: '0',
+        amount: 123,
+        products: [
+          {
+            id: 'productId',
+            name: 'LINE',
+            imageUrl:
+              'https://upload.wikimedia.org/wikipedia/commons/2/2e/LINE_New_App_Icon_%282020-12%29.png',
+            quantity: 2,
+            price: 101
+          }
+        ]
+      }
+    ],
+    redirectUrls: {
+      confirmUrl: 'https://www.google.com',
+      cancelUrl: 'https://www.google.com'
+    }
+  }
+
+  it('should call httpClient.post', async () => {
+    const httpClient = mockHttpClient
+
+    mockHttpClient.post.mockReturnValueOnce(Promise.resolve({ data: {} }))
+
+    request(httpClient)(body)
+
+    expect(httpClient.post).toHaveBeenCalledWith('/v3/payments/request', body)
+
+    expect(httpClient.post).toHaveBeenCalledTimes(1)
+  })
+})

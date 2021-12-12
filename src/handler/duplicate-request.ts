46426d9fbbb7b7a9cd5c4811c321c71784f561d2
Sean Lin <sean.enylin@gmail.com>
2021-12-13 00:05:05 +0800

    feat(handler): add duplicate request handler

---
+++ src/handler/duplicate-request.ts
@@ -0,0 +1,95 @@
+import {
+  ConfirmRequestConfig,
+  ConfirmResponseBody
+} from '../line-pay-api/confirm'
+import {
+  PaymentDetailsResponseBody,
+  paymentDetailsWithClient
+} from '../line-pay-api/payment-details'
+import { RefundRequestConfig, RefundResponseBody } from '../line-pay-api/refund'
+import { createPaymentApi } from '../payment-api/create'
+import { isLinePayApiError } from '../line-pay-api/error/line-pay-api'
+import { ApiHandler, ApiResponse } from '../payment-api/type'
+
+export function toConfirmResponse<
+  Req extends ConfirmRequestConfig | RefundRequestConfig
+>(req: Req, paymentDetails: PaymentDetailsResponseBody): ConfirmResponseBody {
+  const { transactionId } = req
+  const info = paymentDetails.info.find(i => i.transactionId === transactionId)
+
+  if (!info) throw new Error('Transaction ID not found in payment details')
+
+  return {
+    returnCode: paymentDetails.returnCode,
+    returnMessage: paymentDetails.returnMessage,
+    info
+  }
+}
+
+export function toRefundResponse<
+  Req extends ConfirmRequestConfig | RefundRequestConfig
+>(req: Req, paymentDetails: PaymentDetailsResponseBody): RefundResponseBody {
+  const { transactionId } = req
+  const info = paymentDetails.info.find(i => i.transactionId === transactionId)
+
+  if (!info) throw new Error('Transaction ID not found in payment details')
+
+  return {
+    returnCode: paymentDetails.returnCode,
+    returnMessage: paymentDetails.returnMessage,
+    info: {
+      refundTransactionId: transactionId,
+      refundTransactionDate: info.transactionDate
+    }
+  }
+}
+
+/**
+ * Create a handler for confirm and refund API. The handler will handle the 1172 and 1198 error by calling the payment details API and verify the transaction result.
+ *
+ * @param converter convert payment details to response body (confirm/refund)
+ * @returns API handler
+ */
+export const createDuplicateRequestHandler =
+  <
+    Req extends ConfirmRequestConfig | RefundRequestConfig,
+    Res extends ConfirmResponseBody | RefundResponseBody
+  >(
+    converter: (
+      req: Req,
+      paymentDetailsResponseBody: PaymentDetailsResponseBody
+    ) => Res
+  ): ApiHandler<Req, ApiResponse<Res>> =>
+  async (req, next, httpClient) => {
+    try {
+      return await next(req)
+    } catch (e) {
+      // 1172: There is a record of transaction with the same order number.
+      // 1198: API call request has been duplicated.
+      if (
+        isLinePayApiError(e) &&
+        (e.data.returnCode === '1172' || e.data.returnCode === '1198')
+      ) {
+        const paymentDetails = createPaymentApi(
+          paymentDetailsWithClient,
+          httpClient
+        )
+
+        try {
+          const paymentDetailsResponse = await paymentDetails.send({
+            params: {
+              transactionId: [req.transactionId]
+            }
+          })
+
+          return {
+            body: converter(req, paymentDetailsResponse.body),
+            comments: {}
+          }
+        } catch (paymentDetailsError) {
+          throw e
+        }
+      }
+      throw e
+    }
+  }

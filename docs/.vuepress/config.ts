16143073ce80dae920b6633ce7481402acd1edb3
Sean Lin <sean.enylin@gmail.com>
2021-12-21 21:34:30 +0800

    ci(docs): add deployment action

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
   // site config
+  base: '/line-pay-merchant/',
   lang: 'en-US',
   title: 'LINE Pay Merchant',
   description: 'LINE Pay V3 Online APIs library for Node.js',

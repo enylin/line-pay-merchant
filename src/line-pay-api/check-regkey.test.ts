1325d304bc54f4660f27ef1fce022346930c7317
Sean Lin <sean.enylin@gmail.com>
2021-12-27 18:53:59 +0800

    test(check-regkey): test check RegKey API

---
+++ src/line-pay-api/check-regkey.test.ts
@@ -0,0 +1,39 @@
+import {
+  CheckRegKeyRequestConfig,
+  CheckRegKeyRequestParams,
+  checkRegKeyWithClient
+} from './check-regkey'
+
+const mockHttpClient = {
+  get: jest.fn(),
+  post: jest.fn()
+}
+
+describe('check-regkey', () => {
+  const regKey = 'RK9D2BA19XTFQWC'
+
+  const params: CheckRegKeyRequestParams = {}
+
+  it('should call httpClient.get', async () => {
+    const httpClient = mockHttpClient
+
+    mockHttpClient.get.mockReturnValueOnce(Promise.resolve({ data: {} }))
+
+    const req: CheckRegKeyRequestConfig = {
+      regKey,
+      params
+    }
+
+    checkRegKeyWithClient(httpClient)(req)
+
+    expect(httpClient.get).toHaveBeenCalledWith(
+      `/v3/payments/preapprovedPay/${regKey}/check`,
+      {
+        ...req,
+        timeout: 20000
+      }
+    )
+
+    expect(httpClient.get).toHaveBeenCalledTimes(1)
+  })
+})

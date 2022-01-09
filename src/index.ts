d83e6eb1c64a7457f183d264fe2501b6fda2c4e0
Sean Lin <sean.enylin@gmail.com>
2022-01-09 19:03:55 +0800

    feat(type): export API types

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,118 @@ import { checkRegKeyWithClient } from './line-pay-api/check-regkey'
 import { expireRegKeyWithClient } from './line-pay-api/expire-regkey'
 import { voidWithClient } from './line-pay-api/void'
 
+export type {
+  Package as RequestPackage,
+  RedirectUrls,
+  Payment,
+  Display,
+  Shipping as RequestShipping,
+  AddFriend,
+  FamilyService,
+  Extra,
+  Options as RequestOptions,
+  RequestResponseBody,
+  RequestRequestConfig,
+  PaymentUrl,
+  Info as RequestInfo,
+  RequestRequestBody
+} from './line-pay-api/request'
+
+export type {
+  Options as CaptureOptions,
+  CaptureRequestBody,
+  CaptureRequestConfig,
+  PayInfo as CapturePayInfo,
+  Info as CaptureInfo,
+  CaptureResponseBody
+} from './line-pay-api/capture'
+
+export type {
+  CheckPaymentStatusRequestParams,
+  CheckPaymentStatusRequestConfig,
+  Shipping as CheckPaymentStatusShipping,
+  Info as CheckPaymentStatusInfo,
+  CheckPaymentStatusResponseBody
+} from './line-pay-api/check-payment-status'
+
+export type {
+  CheckRegKeyRequestParams,
+  CheckRegKeyRequestConfig
+} from './line-pay-api/check-regkey'
+
+export type {
+  ConfirmRequestBody,
+  ConfirmRequestConfig,
+  PayInfo as ConfirmPayInfo,
+  Package as ConfirmPackage,
+  Shipping as ConfirmShipping,
+  Info as ConfirmInfo,
+  ConfirmResponseBody
+} from './line-pay-api/confirm'
+
+export type {
+  ExpireRegKeyRequestBody,
+  ExpireRegKeyRequestConfig,
+  ExpireRegKeyResponseBody
+} from './line-pay-api/expire-regkey'
+
+export type {
+  PayPreapprovedRequestBody,
+  PayPreapprovedRequestConfig,
+  Info as PayPreapprovedInfo,
+  PayPreapprovedResponseBody
+} from './line-pay-api/pay-preapproved'
+
+export type {
+  Fields,
+  PaymentDetailsRequestParams,
+  PaymentDetailsRequestConfig,
+  PayInfo as PaymentDetailsPayInfo,
+  Refund,
+  Shipping as PaymentDetailsShipping,
+  Package as PaymentDetailsPackage,
+  Event,
+  Info as PaymentDetailsInfo,
+  PaymentDetailsResponseBody
+} from './line-pay-api/payment-details'
+
+export type {
+  RefundRequestBody,
+  RefundRequestConfig,
+  Info as RefundInfo,
+  RefundResponseBody
+} from './line-pay-api/refund'
+
+export type {
+  VoidRequestBody,
+  VoidRequestConfig,
+  VoidResponseBody
+} from './line-pay-api/void'
+
+export type {
+  RequestConfig,
+  ResponseBody,
+  ApiHandlerParams,
+  ApiHandler,
+  ApiResponse,
+  PaymentApi
+} from './payment-api/type'
+
+export type {
+  QueryParams,
+  EmptyObject,
+  LineMerchantConfig,
+  HttpResponse,
+  GeneralRequestConfig,
+  GeneralResponseBody,
+  HttpConfig,
+  HttpClient,
+  Recipient,
+  Address,
+  Product,
+  Currency
+} from './line-pay-api/type'
+
 /**
  * Create a client for LINE Pay API.
  *
